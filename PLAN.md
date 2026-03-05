44dd6a391da7a25150da52c0817bedb9eb7bb2c6
Federico Bartolomei <federico@permutive.com>
2026-03-05 18:37:26 +0000

    setup consumer/producer streams (with circular buffer queue)

--- PLAN.md
+++ PLAN.md
@@ -1,5 +1,11 @@
 ## Notes for next steps
 
+- Consider replacing circular buffer with bounded queue + backpressure (or Channel).
+  One scenario where drop-oldest could make sense is a live/streaming system where freshness matters more than
+  completeness
+  (e.g., real-time data / live feeds). This is currently a batch job processing a finite set of files,
+  so I feel backpressure is more sound given the current assumptions.
+
 - Once the core requirements are done, consider some integration-tests (possibly weaver?) for the overall flow, e.g.
   with wiremock for url resolution
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,43 @@
-import cats.effect.{IO, IOApp}
+import cats.effect.std.Queue
+import cats.effect.{IO, IOApp, Resource}
 import fs2.io.file.Path
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import sttp.client4.httpclient.cats.HttpClientCatsBackend
+import wle.domain.{Hyperlink, RawMarkup}
+import wle.extract.LinkExtractor
 import wle.producer.{Source, UrlFetcher}
 
 object Main extends IOApp.Simple {
   implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+  private val queueCapacity: Int = 10
 
-  override def run: IO[Unit] =
-    HttpClientCatsBackend.resource[IO]().use { backend =>
+  override def run: IO[Unit] = {
+    (for {
+      backend <- HttpClientCatsBackend.resource[IO]()
+      queue <- Resource.eval(
+        // TODO[FB] Not convinced on dropping urls silently here,
+        //  possibly a better idea is to have a bounded queue with backpressure
+        Queue.circularBuffer[IO, Option[RawMarkup]](queueCapacity)
+      )
+    } yield (backend, queue)).use({ case (backend, queue) =>
       val urlFetcher = UrlFetcher.impl[IO](backend)
-      Source
-        .fromPath[IO](Path("src/main/resources"))
-        .urlStream
-        .through(urlFetcher.fetch)
-        .evalTap(markup => logger.info(s"TODO: process markup: [$markup]"))
-        .compile
-        .drain
-    }
+      val producer: fs2.Stream[IO, Unit] =
+        Source
+          .fromPath[IO](Path("src/main/resources"))
+          .urlStream
+          .through(urlFetcher.fetch)
+          .evalMap(rm => queue.offer(Some(rm)))
+          .onFinalize(queue.offer(None))
 
+      val consumer: fs2.Stream[IO, Hyperlink] =
+        fs2.Stream
+          .fromQueueNoneTerminated(queue)
+          .map(LinkExtractor.extract)
+          .flatMap(fs2.Stream.emits)
+          .evalTap(href => logger.info(s"TODO: Process href: [$href]"))
+
+      consumer.concurrently(producer).compile.drain
+    })
+  }
 }
